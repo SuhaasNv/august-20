15ed8cc27acfd4ede4c8ed8e3fce9c7ffac6307f
Suhaas Nv <suhaasnvs@gmail.com>
2025-08-20 14:40:14 +0800

    update

--- src/test/java/sg/edu/nus/iss/d13revision/D13revisionApplicationTests.java
+++ src/test/java/sg/edu/nus/iss/d13revision/D13revisionApplicationTests.java
@@ -1,13 +1,40 @@
 package sg.edu.nus.iss.d13revision;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import sg.edu.nus.iss.d13revision.controllers.DataController; // Make sure this import is correct for your package structure
 
 @SpringBootTest
 class D13revisionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    DataController dataController;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void health() {
+        assertEquals("HEALTH CHECK OK!", dataController.healthCheck());
+    }
+
+    @Test
+    void version() {
+        assertEquals("The actual version is 1.0.0", dataController.version());
+    }
+
+    @Test
+    void nationsLength() {
+        Integer nationsLength = dataController.getRandomNations().size();
+        assertEquals(10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = dataController.getRandomCurrencies().size();
+        assertEquals(20, currenciesLength);
+    }
 }
